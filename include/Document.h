4e6a8c0855e910dc91e288a671c74c25f04bda8c
Martino Lai <140235799+martinolai@users.noreply.github.com>
2025-08-23 14:23:11 +0200

    feat: add Document class header for searchable documents
    - Define Document class with id, title, content, and url fields
    - Provide constructor declaration
    - Serve as core metadata container for the search engine

--- include/Document.h
+++ include/Document.h
@@ -1,1 +1,21 @@
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <string>
+using namespace std;
+
+/**
+ * Document: Represents a searchable document with metadata
+ */
+class Document {
+public:
+    int id;
+    string title;
+    string content;
+    string url;
+
+    Document(int id, const string& title, const string& content, const string& url = "");
+};
+
+#endif
 
